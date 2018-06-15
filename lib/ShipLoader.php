6a202d9ba23cf48d7eedf078553dc477d2a6bf6c
Karim <abdounikarim@gmail.com>
2018-06-16 01:59:09 +0200

    fetching objects from the database

--- lib/ShipLoader.php
+++ lib/ShipLoader.php
@@ -4,32 +4,29 @@ class ShipLoader
 {
     public function getShips()
     {
+        $shipsData = $this->queryForShips();
+
         $ships = [];
 
-        $ship1 = new Ship('Jedi Starfighter');
-        $ship1->setWeaponPower(5);
-        $ship1->setJediFactor(15);
-        $ship1->setStrength(30);
-        $ships['starfighter'] = $ship1;
-
-        $ship2 = new Ship('CloakShape Fighter');
-        $ship2->setWeaponPower(2);
-        $ship2->setJediFactor(2);
-        $ship2->setStrength(70);
-        $ships['cloakshape_fighter'] = $ship2;
-
-        $ship3 = new Ship('Super Star Destroyer');
-        $ship3->setWeaponPower(70);
-        $ship3->setJediFactor(0);
-        $ship3->setStrength(500);
-        $ships['super_star_destroyer'] = $ship3;
-
-        $ship4 = new Ship('RZ-1 A-wing interceptor');
-        $ship4->setWeaponPower(4);
-        $ship4->setJediFactor(4);
-        $ship4->setStrength(50);
-        $ships['rz1_a_wing_interceptor'] = $ship4;
+        foreach ($shipsData as $shipData) {
+            $ship = new Ship($shipData['name']);
+            $ship->setWeaponPower($shipData['weapon_power']);
+            $ship->setJediFactor($shipData['jedi_factor']);
+            $ship->setStrength($shipData['strength']);
+            $ships[] = $ship;
+        }
 
         return $ships;
+    }
+
+    private function queryForShips()
+    {
+        $pdo = new PDO('mysql:host=localhost;dbname=oo_battle', 'root', 'root');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $statement = $pdo->prepare('SELECT * FROM ship');
+        $statement->execute();
+        $shipsArray = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        return $shipsArray;
     }
 }
