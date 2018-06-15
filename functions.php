af56e839e405c3ff0cf83b60d172d5c4df6bfee9
Karim <abdounikarim@gmail.com>
2018-06-16 00:39:11 +0200

    the constructor

--- functions.php
+++ functions.php
@@ -6,12 +6,30 @@ function get_ships()
 {
     $ships = [];
 
-    $ship1 = new Ship();
-    $ship1->setName('Jedi Starfighter');
+    $ship1 = new Ship('Jedi Starfighter');
     $ship1->setWeaponPower(5);
     $ship1->setJediFactor(15);
     $ship1->setStrength(30);
     $ships['starfighter'] = $ship1;
+
+    $ship2 = new Ship('CloakShape Fighter');
+    $ship2->setWeaponPower(2);
+    $ship2->setJediFactor(2);
+    $ship2->setStrength(70);
+    $ships['cloakshape_fighter'] = $ship2;
+
+    $ship3 = new Ship('Super Star Destroyer');
+    $ship3->setWeaponPower(70);
+    $ship3->setJediFactor(0);
+    $ship3->setStrength(500);
+    $ships['super_star_destroyer'] = $ship3;
+
+    $ship4 = new Ship('RZ-1 A-wing interceptor');
+    $ship4->setWeaponPower(4);
+    $ship4->setJediFactor(4);
+    $ship4->setStrength(50);
+    $ships['rz1_a_wing_interceptor'] = $ship4;
+
     return $ships;
 }
 
--- lib/Ship.php
+++ lib/Ship.php
@@ -10,6 +10,15 @@ class Ship
 
     private $strength = 0;
 
+    private $underRepair;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+        $this->underRepair = mt_rand(1, 100) < 30;
+    }
+
+
     public function sayHello()
     {
         echo 'Hello';
@@ -99,5 +108,11 @@ class Ship
         $this->name = $name;
     }
 
-
+    /**
+     * @return bool
+     */
+    public function isFunctional()
+    {
+        return $this->underRepair;
+    }
 }
