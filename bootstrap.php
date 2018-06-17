89398a1397ac09bfb8d56c8846e562d43c6ef0b3
Karim <abdounikarim@gmail.com>
2018-06-17 19:33:35 +0200

    service container

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,7 @@ require __DIR__.'/lib/Ship.php';
 require __DIR__.'/lib/ShipLoader.php';
 require __DIR__.'/lib/BattleManager.php';
 require __DIR__.'/lib/BattleResult.php';
+require __DIR__.'/lib/Container.php';
 
 $configuration = [
     'db_dsn' => 'mysql:host=localhost;dbname=oo_battle',
