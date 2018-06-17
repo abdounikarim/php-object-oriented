77f03a2d3f343790c89e032225894837348e7e4b
Karim <abdounikarim@gmail.com>
2018-06-17 16:50:15 +0200

    oo best practice: centralizing configuration

--- bootstrap.php
+++ bootstrap.php
@@ -4,3 +4,9 @@ require __DIR__.'/lib/Ship.php';
 require __DIR__.'/lib/ShipLoader.php';
 require __DIR__.'/lib/BattleManager.php';
 require __DIR__.'/lib/BattleResult.php';
+
+$configuration = [
+    'db_dsn' => 'mysql:host=localhost;dbname=oo_battle',
+    'db_user' => 'root',
+    'db_pass' => 'root'
+];
