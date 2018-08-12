8b9177d5b64a0b5b0a00706c34cd96132e532c1f
Karim <abdounikarim@gmail.com>
2018-08-12 16:52:06 +0200

    autoloading awesomeness

--- bootstrap.php
+++ bootstrap.php
@@ -8,9 +8,16 @@ require __DIR__ . '/lib/Service/ShipStorageInterface.php';
 require __DIR__ . '/lib/Service/PDOShipStorage.php';
 require __DIR__ . '/lib/Service/JsonFileShipStorage.php';
 require __DIR__ . '/lib/Service/ShipLoader.php';
-require __DIR__ . '/lib/Service/BattleManager.php';
 require __DIR__ . '/lib/Model/BattleResult.php';
 require __DIR__ . '/lib/Service/Container.php';
+
+spl_autoload_register(function ($className) {
+    $path = __DIR__.'/lib/'.str_replace('\\', '/', $className).'.php';
+
+    if(file_exists($path)) {
+        require $path;
+    }
+});
 
 $configuration = [
     'db_dsn' => 'mysql:host=localhost;dbname=oo_battle',
