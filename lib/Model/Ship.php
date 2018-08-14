3ae78a4991adcdea0c48ec2c1ee9c5b0e31f8b1d
Karim <abdounikarim@gmail.com>
2018-08-14 19:52:58 +0200

    traits: "horizontal" reuse

--- lib/Model/Ship.php
+++ lib/Model/Ship.php
@@ -4,39 +4,18 @@ namespace Model;
 
 class Ship extends AbstractShip
 {
-    private $jediFactor = 0;
+    use SettableJediFactorTrait;
 
     private $underRepair;
 
-    /**
-     * Ship constructor.
-     * @param $underRepair
-     */
     public function __construct($name)
     {
         parent::__construct($name);
+
+        // randomly put this ship under repair
         $this->underRepair = mt_rand(1, 100) < 30;
     }
 
-    /**
-     * @return int
-     */
-    public function getJediFactor()
-    {
-        return $this->jediFactor;
-    }
-
-    /**
-     * @param int $jediFactor
-     */
-    public function setJediFactor($jediFactor)
-    {
-        $this->jediFactor = $jediFactor;
-    }
-
-    /**
-     * @return bool
-     */
     public function isFunctional()
     {
         return !$this->underRepair;
